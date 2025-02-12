fc0578087c606eb30729599ee0928c101d8d2ccd
loop-hack <timetraveller1732@gmail.com>
2025-02-12 15:41:03 +0530

    created front page

--- script.js
+++ script.js
@@ -13,3 +13,48 @@ document.getElementById('registerForm').addEventListener('submit', function (eve
     alert('Please fill in all fields!');
   }
 });
+
+// GSAP Animations
+gsap.registerPlugin(ScrollTrigger);
+
+// Fade-in animation for the heading
+gsap.from(".c-heading__middle h3", {
+  duration: 1,
+  y: 50,
+  opacity: 0,
+  ease: "power3.out",
+  delay: 0.5,
+});
+
+// Fade-in animation for the credits
+gsap.from(".users-credits ul li", {
+  duration: 1,
+  y: 30,
+  opacity: 0,
+  stagger: 0.2,
+  ease: "power3.out",
+  delay: 1,
+});
+
+// Scroll-triggered animations
+gsap.from(".c-heading__top h2", {
+  scrollTrigger: {
+    trigger: ".c-heading__top",
+    start: "top 80%",
+  },
+  y: 30,
+  opacity: 0,
+  duration: 1,
+  ease: "power3.out",
+});
+
+gsap.from(".c-heading__bottom", {
+  scrollTrigger: {
+    trigger: ".c-heading__bottom",
+    start: "top 80%",
+  },
+  y: 30,
+  opacity: 0,
+  duration: 1,
+  ease: "power3.out",
+});
